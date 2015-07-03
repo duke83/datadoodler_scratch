6d3c60e8d586c6b3a27ff488b84ac50adb463790
Kent Merrell <kmerrell@nerium.com>
2015-07-02 20:24:39 -0400

    modfied mock datasets

--- Framework/app/services/dataService.js
+++ Framework/app/services/dataService.js
@@ -4,18 +4,12 @@ angular.module('app').factory('dataService',
     ['$timeout',
     function ($timeout) {
 
-        var datasets = [
-            {
-                id: "ds1",
-                name: "Arnold Schwarzenegger Movies"
-            }
-
-        ]
 
 
         var ArnoldSchwarzeneggerMovies_Glossary = [{
-            Source: "https://en.wikipedia.org/wiki/Arnold_Schwarzenegger_filmography",
-            Processing = "http://www.convertcsv.com/csv-to-json.htm",
+            "Name":"Arnold Schwarzenegger Movies",
+            "Source": "https://en.wikipedia.org/wiki/Arnold_Schwarzenegger_filmography",
+            "Processing ": "1) Kent Merrell copied data from a table on Wikipedia and pasted into a googlesheet.  2)Export to CSV 3) Convert CSV to JSON with http://www.convertcsv.com/csv-to-json.htm",
             SourceDate: "July 1, 2015",
             Variables:[
             { "VarName": "Title", "ShortDescription": "Title of movie.", "LongDescription": "" },
@@ -25,8 +19,9 @@ angular.module('app').factory('dataService',
             { "VarName": "Studio(s)", "ShortDescription": "Name of the movie studio that produced movie.", "LongDescription": "" },
             { "VarName": "Rotten Tomatoes", "ShortDescription": "Rating from Rotten Tomatoes movie rating service", "LongDescription": "" },
             { "VarName": "Notes", "ShortDescription": "Title of movie", "LongDescription": "" }
-        ]
-        }]
+            ]
+        }];
+
         var ArnoldSchwarzeneggerMovies_Data = [
       {
           "Title": "Twins",
@@ -444,8 +439,21 @@ angular.module('app').factory('dataService',
       }
         ];
 
+        var JamieLeeCurtisMovies_Glossary = [{
+            "Name":"Jamie Lee Curtis Movies",
+            "Source": "https://en.wikipedia.org/wiki/Arnold_Schwarzenegger_filmography",
+            "Processing ": "1) Kent Merrell copied data from a table on Wikipedia and pasted into a googlesheet.  2)Export to CSV 3) Convert CSV to JSON with http://www.convertcsv.com/csv-to-json.htm",
+            "SourceDate": "July 1, 2015",
+            "Variables":[
+            { "VarName": "Title", "ShortDescription": "Title of movie.", "LongDescription": "" },
+            { "VarName": "Year", "ShortDescription": "The year the movie was released.", "LongDescription": "" },
+            { "VarName": "Role", "ShortDescription": "The rolw that Arnold Schwarzenegger played.", "LongDescription": "" },
+            { "VarName": "Notes", "ShortDescription": "Title of movie", "LongDescription": "" }
+            ]
+        }];
 
-        var JamieLeeCurtisMovies = [
+
+        var JamieLeeCurtisMovies_Data = [
   {
       "Year": "1978",
       "Title": "Halloween",
@@ -706,153 +714,176 @@ angular.module('app').factory('dataService',
   }
         ]
 
-    var locations = [
-        {
-            id: 1000,
-            name: 'Raquette Riverx',
-            temperature: 55,
-            guides: 20,
-            rafts: 18,
-            vests: 200,
-            image: 'river1.png'
-        },
-        {
-            id: 1001,
-            name: 'Black River',
-            temperature: 53,
-            guides: 36,
-            rafts: 22,
-            vests: 250,
-            image: 'river2.png'
-        },
-        {
-            id: 1002,
-            name: 'Hudson River',
-            temperature: 58,
-            guides: 56,
-            rafts: 40,
-            vests: 500,
-            image: 'river3.png'
-        },
-        {
-            id: 1003,
-            name: 'Hudson Gorge',
-            temperature: 39,
-            guides: 8,
-            rafts: 10,
-            vests: 40,
-            image: 'river4.png'
-        },
-        {
-            id: 1004,
-            name: 'Saranac River',
-            temperature: 32,
-            guides: 8,
-            rafts: 8,
-            vests: 100,
-            image: 'river1.png'
-        },
-        {
-            id: 1005,
-            name: 'Black Creek',
-            temperature: 34,
-            guides: 22,
-            rafts: 12,
-            vests: 230,
-            image: 'river2.png'
-        },
-        {
-            id: 1006,
-            name: 'Batten Kill',
-            temperature: 54,
-            guides: 20,
-            rafts: 24,
-            vests: 420,
-            image: 'river3.png'
-        },
-        {
-            id: 1007,
-            name: 'Ausable River',
-            temperature: 38,
-            guides: 12,
-            rafts: 8,
-            vests: 225,
-            image: 'river4.png'
-        }
-    ];
+        var locations = [
+            {
+                id: 1000,
+                name: 'Raquette Riverx',
+                temperature: 55,
+                guides: 20,
+                rafts: 18,
+                vests: 200,
+                image: 'river1.png'
+            },
+            {
+                id: 1001,
+                name: 'Black River',
+                temperature: 53,
+                guides: 36,
+                rafts: 22,
+                vests: 250,
+                image: 'river2.png'
+            },
+            {
+                id: 1002,
+                name: 'Hudson River',
+                temperature: 58,
+                guides: 56,
+                rafts: 40,
+                vests: 500,
+                image: 'river3.png'
+            },
+            {
+                id: 1003,
+                name: 'Hudson Gorge',
+                temperature: 39,
+                guides: 8,
+                rafts: 10,
+                vests: 40,
+                image: 'river4.png'
+            },
+            {
+                id: 1004,
+                name: 'Saranac River',
+                temperature: 32,
+                guides: 8,
+                rafts: 8,
+                vests: 100,
+                image: 'river1.png'
+            },
+            {
+                id: 1005,
+                name: 'Black Creek',
+                temperature: 34,
+                guides: 22,
+                rafts: 12,
+                vests: 230,
+                image: 'river2.png'
+            },
+            {
+                id: 1006,
+                name: 'Batten Kill',
+                temperature: 54,
+                guides: 20,
+                rafts: 24,
+                vests: 420,
+                image: 'river3.png'
+            },
+            {
+                id: 1007,
+                name: 'Ausable River',
+                temperature: 38,
+                guides: 12,
+                rafts: 8,
+                vests: 225,
+                image: 'river4.png'
+            }
+        ];
 
-    var employees = [
-        {
-            id: 5000,
-            name: 'Andy Chatterton',
-            location: 'Raquette River',
-            image: 'employee-andy.png'
-        },
-        {
-            id: 5001,
-            name: 'April Donaldson',
-            location: 'Saranac River',
-            image: 'employee-april.png'
-        },
-        {
-            id: 5002,
-            name: 'Don Morgan',
-            location: 'Black Creek',
-            image: 'employee-don.png'
-        },
-        {
-            id: 5003,
-            name: 'Tom Sullivan',
-            location: 'Ausable River',
-            image: 'employee-tom.png'
-        },
-        {
-            id: 5004,
-            name: 'Kathy Fletcher',
-            location: 'Batten Kill',
-            image: 'employee-kathy.png'
-        }
-    ];
+        var employees = [
+            {
+                id: 5000,
+                name: 'Andy Chatterton',
+                location: 'Raquette River',
+                image: 'employee-andy.png'
+            },
+            {
+                id: 5001,
+                name: 'April Donaldson',
+                location: 'Saranac River',
+                image: 'employee-april.png'
+            },
+            {
+                id: 5002,
+                name: 'Don Morgan',
+                location: 'Black Creek',
+                image: 'employee-don.png'
+            },
+            {
+                id: 5003,
+                name: 'Tom Sullivan',
+                location: 'Ausable River',
+                image: 'employee-tom.png'
+            },
+            {
+                id: 5004,
+                name: 'Kathy Fletcher',
+                location: 'Batten Kill',
+                image: 'employee-kathy.png'
+            }
+        ];
 
-    var getLocations = function () {
-        return $timeout(function () {
-            return locations;
-        }, 500);
-    };
-
-    var getLocation = function (id) {
-        var timeout = $timeout(function () {
-            //$timeout.cancel(timeout);
-            //return undefined;
-            for (var i = 0; i < locations.length; i++)
-                if (locations[i].id == id)
-                    return locations[i];
-            return undefined;
-        }, 300);
-
-        return timeout;
-    };
-
-    var getEmployees = function () {
-        return $timeout(function () {
-            return employees;
-        }, 500);
-    };
-
-    var getEmployee = function (id) {
-        return $timeout(function () {
-            for (var i = 0; i < employees.length; i++)
-                if (employees[i].id == id)
-                    return employees[i];
-            return undefined;
-        }, 300);
-    };
+        var getArnoldSchwarzeneggerMovies_Glossary = function () {
+            return $timeout(function () {
+                return JamieLeeCurtisMovies_Glossary;
+            }, 500);
+        };
+        var getArnoldSchwarzeneggerMovies_Data= function () {
+            return $timeout(function () {
+                return getJamieLeeCurtisMovies();
+            }, 500);
+        };
 
 
-    return {
-        getLocations: getLocations,
-        getLocation: getLocation,
-        getEmployees: getEmployees,
-        getEmployee: getEmployee
-    };
-}]);
+        var getJamieLeeCurtisMovies_Glossary = function () {
+            return $timeout(function () {
+                return JamieLeeCurtisMovies_Glossary;
+            }, 500);
+        };
+        var getJamieLeeCurtisMovies_Data = function () {
+            return $timeout(function () {
+                return getJamieLeeCurtisMovies();
+            }, 500);
+        };
+
+        var getLocations = function () {
+            return $timeout(function () {
+                return locations;
+            }, 500);
+        };
+
+        var getLocation = function (id) {
+            var timeout = $timeout(function () {
+                //$timeout.cancel(timeout);
+                //return undefined;
+                for (var i = 0; i < locations.length; i++)
+                    if (locations[i].id == id)
+                        return locations[i];
+                return undefined;
+            }, 300);
+
+            return timeout;
+        };
+
+        var getEmployees = function () {
+            return $timeout(function () {
+                return employees;
+            }, 500);
+        };
+
+        var getEmployee = function (id) {
+            return $timeout(function () {
+                for (var i = 0; i < employees.length; i++)
+                    if (employees[i].id == id)
+                        return employees[i];
+                return undefined;
+            }, 300);
+        };
+
+
+        return {
+            getLocations: getLocations,
+            getLocation: getLocation,
+            getEmployees: getEmployees,
+            getEmployee: getEmployee
+        };
+    }]);
