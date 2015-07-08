247a1430420931327039e66933bbd149a78c4c4e
Kent Merrell <kmerrell@gmail.com>
2015-07-08 17:00:10 -0400

    Customized SASS and Gulp usage.

---
+++ config/assets/test.js
@@ -0,0 +1,12 @@
+/**
+ * Created by kdm on 7/8/15.
+ */
+'use strict';
+
+module.exports = {
+    tests: {
+        client: ['modules/*/tests/client/**/*.js'],
+        server: ['modules/*/tests/server/**/*.js'],
+        e2e: ['modules/*/tests/e2e/**/*.js']
+    }
+};
