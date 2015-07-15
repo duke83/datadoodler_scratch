66de22914f7a2a858ae231897debce4bd8b78224
Kent Merrell <kmerrell@gmail.com>
2015-07-15 14:37:22 -0400

    Updates to doodleWidgets array handling

--- public/modules/doodleGlossary/directives/glossaryWidget.directive.js
+++ public/modules/doodleGlossary/directives/glossaryWidget.directive.js
@@ -1,12 +1,14 @@
 'use strict';
 
-angular.module('doodleGlossary').directive('glossaryWidget',function(){
-    return{
-        restrict:'E',
-        templateUrl:'modules/doodleGlossary/directives/glossaryWidget.directive.html',
-        scope:{widgetitem:"="},
-        link:function(scp,el,att){
-
+angular.module('doodleGlossary').directive('glossaryWidget', function () {
+    return {
+        restrict: 'E',
+        templateUrl: 'modules/doodleGlossary/directives/glossaryWidget.directive.html',
+        scope: {widgetitem: "="},
+        link: function (scp, el, att) {
+            scp.checkboxChange = function (glossaryVariable) {
+                console.log(glossaryVariable);
+            }
         }
         //template:'<h1>this is a template (not a templateUrl) for a datawidget</h1>'
     };
