91ccc009c053f1f05fc293eeb0d4377b0139129b
Kent Merrell <kmerrell@gmail.com>
2015-07-30 15:57:23 -0400

    about to start banker doodle.

--- public/modules/doodlecalc/directives/calcwidget.directive.js
+++ public/modules/doodlecalc/directives/calcwidget.directive.js
@@ -4,6 +4,12 @@ angular.module('doodlecalc').directive('calcwidget',function(){
     return{
         restrict:'E',
         templateUrl:'modules/doodlecalc/directives/calcwidget.directive.html',
-        scope: {widgetitem: "="}
+        scope: {widgetitem: "="},
+        link:function(scp,el,attrs){
+            scp.vars=[{name:'Title', id:'v123441',pk:'Title',type:'string',
+            rows:['Twins','True Lies','Total Recall','The Villain',
+                'The Terminator', 'The Running Man']
+            }]
+        }
     };
 });
